8a67a1d70733b0c888403e26d45144374e41c2d3
Tiobilito <andre.marin1135@alumnos.udg.mx>
2024-04-09 12:09:16 -0600

    removing verifycr.js from proyect

--- Scenes/VerifyCr.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-//import { StyleSheet, Text, Image, TouchableOpacity, View } from 'react-native';
-
-const VerifyCrPage = ({navigation}) => {
-    
-}
-
-export default VerifyCrPage;
