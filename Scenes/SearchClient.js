18309a27e4fedd306ef71bef1acc9e41ab0d87e3
Tiobilito <andre.marin1135@alumnos.udg.mx>
2024-04-17 17:49:23 -0600

    Stap Snav

--- Scenes/SearchClient.js
+++ Scenes/SearchClient.js
@@ -80,8 +80,8 @@ const SearchPage = ({navigation}) => {
         }
     }
 
-    const navigateToEditClient = async (idCli) => {
-      navigation.navigate("EditClient", { idClient: idCli});
+    const navigateToEditClient = (id) => {
+      navigation.navigate("EditClient", { idClient: id});
     }
 
     return (
@@ -124,7 +124,7 @@ const SearchPage = ({navigation}) => {
                             <Text style = {styles.textSrt}>Telefono: {item.telefono}</Text>
                             <Text style = {styles.textSrt}>2do Telefono: {item.telefono2}</Text>
                               <View style = {styles.buttoms}>
-                                <TouchableOpacity onPress={() => {navigateToEditClient(item.id)}}>
+                                <TouchableOpacity onPress={() => {navigateToEditClient(item.idCliente)}}>
                                     <Image
                                         source = {require('../Resources/imagenes/editar.png')}
                                         style = {styles.image}
