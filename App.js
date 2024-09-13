4e54fce99ce87a2dec3d66a89a0236c93d12480e
Tiobilito <andre.marin1135@alumnos.udg.mx>
2024-09-13 09:43:21 -0600

    Se implemento un tab navigator para worker

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-//Scenes
+// Scenes
 import LoggingPage from "./Scenes/Logging";
 import WorkerPage from "./Scenes/WorkerUser/Worker";
 import AddClientPage from "./Scenes/WorkerUser/AddClient";
@@ -14,6 +16,56 @@ import EditDevicePage from "./Scenes/WorkerUser/EditDevice";
 import OrderPage from "./Scenes/WorkerUser/Order";
 
 const Stack = createNativeStackNavigator();
+const WorkerTap = createBottomTabNavigator();
+const MainWorkerStack = createNativeStackNavigator();
+const RepairClientDevicesStack = createNativeStackNavigator();
+
+function Worker() {
+  return (
+    <MainWorkerStack.Navigator screenOptions={{ headerShown: false }}>
+      <MainWorkerStack.Screen name="Worker" component={WorkerPage} />
+      <MainWorkerStack.Screen name="AddClient" component={AddClientPage} />
+    </MainWorkerStack.Navigator>
+  );
+}
+
+function Fixes() {
+  return (
+    <RepairClientDevicesStack.Navigator screenOptions={{ headerShown: false }}>
+      <RepairClientDevicesStack.Screen name="SearchClient" component={SearchClientPage} />
+      <RepairClientDevicesStack.Screen name="Devices" component={DevicesPage} />
+      <RepairClientDevicesStack.Screen name="EditClient" component={EditClientPage} />
+      <RepairClientDevicesStack.Screen name="EditDevice" component={EditDevicePage} />
+      <RepairClientDevicesStack.Screen name="AddDevice" component={AddDevicePage} />
+      <RepairClientDevicesStack.Screen name="Order" component={OrderPage} />
+    </RepairClientDevicesStack.Navigator>
+  );
+}
+
+const WorkerApp = () => {
+  return (
+    <WorkerTap.Navigator
+      initialRouteName="Principal"
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Principal") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Reparaciones") {
+            iconName = focused ? "build" : "build-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "black",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <WorkerTap.Screen name="Principal" component={Worker} />
+      <WorkerTap.Screen name="Reparaciones" component={Fixes} />
+    </WorkerTap.Navigator>
+  );
+};
 
 export default function App() {
   return (
@@ -24,14 +76,7 @@ export default function App() {
         }}
       >
         <Stack.Screen name="Logging" component={LoggingPage} />
-        <Stack.Screen name="Worker" component={WorkerPage} />
-        <Stack.Screen name="AddClient" component={AddClientPage} />
-        <Stack.Screen name="SearchClient" component={SearchClientPage} />
-        <Stack.Screen name="Devices" component={DevicesPage} />
-        <Stack.Screen name="EditClient" component={EditClientPage} />
-        <Stack.Screen name="EditDevice" component={EditDevicePage} />
-        <Stack.Screen name="AddDevice" component={AddDevicePage} />
-        <Stack.Screen name="Order" component={OrderPage} />
+        <Stack.Screen name="WorkerApp" component={WorkerApp} />
       </Stack.Navigator>
     </NavigationContainer>
   );
