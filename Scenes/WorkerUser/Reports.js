a12d66d1bffe6ec99ac76f9213f4bfe3741aabef
Enrique Aguilar <eaguilarg23@gmail.com>
2024-09-26 19:36:48 -0600

    Actualización botones en Worker.js. DevicesTest y Reports son pruebas para Top Tab Nav

---
+++ Scenes/WorkerUser/Reports.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export class Reports extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Reports</Text>
+      </View>
+    );
+  }
+}
+
+export default Reports;
