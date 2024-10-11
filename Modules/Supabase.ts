4ce5e09663ce9ae296d4df78c8a1f539b54977db
Tiobilito <andre.marin1135@alumnos.udg.mx>
2024-10-11 12:51:55 -0600

    Cambio de libreria para supabase.ts, para interactuar con la web

--- Modules/Supabase.ts
+++ Modules/Supabase.ts
@@ -1,18 +1,31 @@
-import * as SecureStore from 'expo-secure-store'
-import { createClient } from '@supabase/supabase-js'
-//import { SupaLink, SupaAnonKey } from '../LocalV'
+import * as SecureStore from 'expo-secure-store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Platform } from 'react-native';
+import { createClient } from '@supabase/supabase-js';
 
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
-    return SecureStore.getItemAsync(key)
+    if (Platform.OS === 'web') {
+      return AsyncStorage.getItem(key);
+    } else {
+      return SecureStore.getItemAsync(key);
+    }
   },
   setItem: (key: string, value: string) => {
-    SecureStore.setItemAsync(key, value)
+    if (Platform.OS === 'web') {
+      return AsyncStorage.setItem(key, value);
+    } else {
+      return SecureStore.setItemAsync(key, value);
+    }
   },
   removeItem: (key: string) => {
-    SecureStore.deleteItemAsync(key)
+    if (Platform.OS === 'web') {
+      return AsyncStorage.removeItem(key);
+    } else {
+      return SecureStore.deleteItemAsync(key);
+    }
   },
-}
+};
 
 //Project URL
 const supabaseUrl = "https://lpxsgoriiklrtkyfoxtk.supabase.co"
@@ -20,10 +33,10 @@ const supabaseUrl = "https://lpxsgoriiklrtkyfoxtk.supabase.co"
 const supabaseAnonKey= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxweHNnb3JpaWtscnRreWZveHRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5MzkyNDUsImV4cCI6MjAzMjUxNTI0NX0.HSSUL1zoy944xHop9X36WGHDvdE4zUe9Y7Fb0GNVUj0"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-      storage: ExpoSecureStoreAdapter as any,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-    },
-  })
+  auth: {
+    storage: ExpoSecureStoreAdapter,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
