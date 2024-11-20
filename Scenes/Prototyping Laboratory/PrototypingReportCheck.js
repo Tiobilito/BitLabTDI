7e5381f089222a4aa271ee0e7f4420dc66431fff
Tiobilito <andre.marin1135@alumnos.udg.mx>
2024-11-20 13:54:38 -0600

    CL

--- Scenes/Prototyping Laboratory/PrototypingReportCheck.js
+++ Scenes/Prototyping Laboratory/PrototypingReportCheck.js
@@ -13,12 +13,7 @@ import {
 } from "react-native";
 import filter from "lodash.filter";
 import { useFocusEffect } from "@react-navigation/native";
-import {
-  getAllProjectSubmissionsCheck,
-  getAllProjectSubmissionsCheckDH,
-  getAllProjectSubmissionsCheckLH,
-  getAllProjectSubmissionsCheckSS,
-} from "../../Modules/OperacionesBD"; // Asegúrate de implementar correctamente esta función
+import { getAllProjectSubmissionsCheck } from "../../Modules/OperacionesBD"; // Asegúrate de implementar correctamente esta función
 import { CustomViewReverse } from "../components/CustomViewReverse";
 import Icon from "react-native-vector-icons/Ionicons";
 import { GetUserData } from "../../Modules/DataInfo";
