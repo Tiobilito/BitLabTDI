a524d1520a46af09a7269842d7edb93f6b76bd9b
Tiobilito <andre.marin1135@alumnos.udg.mx>
2024-11-29 12:09:19 -0600

    Sustitucion de securestore por otra solucion

--- Modules/Supabase.ts
+++ Modules/Supabase.ts
@@ -1,29 +1,43 @@
-import * as SecureStore from 'expo-secure-store'
-import { createClient } from '@supabase/supabase-js'
-//import { SupaLink, SupaAnonKey } from '../LocalV'
+import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Platform } from 'react-native';
 
-const ExpoSecureStoreAdapter = {
-  getItem: (key: string) => {
-    return SecureStore.getItemAsync(key)
+// Crea un adaptador universal para el almacenamiento
+const UniversalStorageAdapter = {
+  getItem: async (key: string) => {
+    if (Platform.OS === 'web') {
+      return localStorage.getItem(key); // Usa localStorage para la web
+    } else {
+      return await AsyncStorage.getItem(key); // Usa AsyncStorage para móviles
+    }
   },
-  setItem: (key: string, value: string) => {
-    SecureStore.setItemAsync(key, value)
+  setItem: async (key: string, value: string) => {
+    if (Platform.OS === 'web') {
+      localStorage.setItem(key, value); // Usa localStorage para la web
+    } else {
+      await AsyncStorage.setItem(key, value); // Usa AsyncStorage para móviles
+    }
   },
-  removeItem: (key: string) => {
-    SecureStore.deleteItemAsync(key)
+  removeItem: async (key: string) => {
+    if (Platform.OS === 'web') {
+      localStorage.removeItem(key); // Usa localStorage para la web
+    } else {
+      await AsyncStorage.removeItem(key); // Usa AsyncStorage para móviles
+    }
   },
-}
+};
 
-//Project URL
-const supabaseUrl = "https://lpxsgoriiklrtkyfoxtk.supabase.co"
-//API Key
-const supabaseAnonKey= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxweHNnb3JpaWtscnRreWZveHRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5MzkyNDUsImV4cCI6MjAzMjUxNTI0NX0.HSSUL1zoy944xHop9X36WGHDvdE4zUe9Y7Fb0GNVUj0"
+// URL del proyecto y clave de API
+const supabaseUrl = 'https://lpxsgoriiklrtkyfoxtk.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxweHNnb3JpaWtscnRreWZveHRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5MzkyNDUsImV4cCI6MjAzMjUxNTI0NX0.HSSUL1zoy944xHop9X36WGHDvdE4zUe9Y7Fb0GNVUj0';
 
+// Configura Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-      storage: ExpoSecureStoreAdapter as any,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-    },
-  })
+  auth: {
+    storage: UniversalStorageAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
