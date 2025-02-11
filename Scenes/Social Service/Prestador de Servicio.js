e1cf83072f6cbf35e3082a74011001ac139546b4
Tiobilito <andre.marin1135@alumnos.udg.mx>
2025-02-11 12:50:56 -0600

    CL

--- Scenes/Social Service/Prestador de Servicio.js
+++ Scenes/Social Service/Prestador de Servicio.js
@@ -218,7 +218,7 @@ const styles = StyleSheet.create({
   },
   tables: {
     width: WIDTH * 0.9,
-    height: HEIGHT * 0.55,
+    height: HEIGHT * 0.45,
     backgroundColor: "#FFFFFF",
     borderRadius: 10,
     margin: 10,
