d3b23fd7a8609b1185cbc16a7d73b595f1702635
Cristian <cristian.orozco9097@alumnos.udg.mx>
2025-03-20 14:03:24 -0600

    Ya no hay error al seleccionar cuenta en GoogleDrivePicker pero no sirve el redireccionamiento

--- components/GoogleDrivePicker.jsx
+++ components/GoogleDrivePicker.jsx
@@ -1,151 +1,94 @@
 import { useEffect, useState } from "react";
-import { View, StyleSheet, Button, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Button,
+  Text,
+  Modal,
+} from "react-native";
 import { WebView } from "react-native-webview";
 import * as WebBrowser from "expo-web-browser";
-// import * as AuthSession from "expo-auth-session";
 import * as Google from "expo-auth-session/providers/google";
+import * as AuthSession from 'expo-auth-session'
 
 WebBrowser.maybeCompleteAuthSession();
 
-const GOOGLE_WEB_TOKEN = "955311692015-k7sbk98ks82e3nv389u4nhvbk69obq0n.apps.googleusercontent.com";
-const GOOGLE_ANDROID_TOKEN = "955311692015-ta1lf17m1h1sak8ce4ljjkb5qc6d7dls.apps.googleusercontent.com";
-const REDIRECT_URI = AuthSession.makeRedirectUri({ useProxy: true });
+const GOOGLE_WEB_TOKEN =
+  "955311692015-k7sbk98ks82e3nv389u4nhvbk69obq0n.apps.googleusercontent.com";
+const GOOGLE_ANDROID_TOKEN =
+  "955311692015-ta1lf17m1h1sak8ce4ljjkb5qc6d7dls.apps.googleusercontent.com";
 
-const SCOPES = [
-  "https://www.googleapis.com/auth/drive.readonly",
-  "https://www.googleapis.com/auth/drive",
-  "profile",
-  "email",
-];
+const SCOPES = ["https://www.googleapis.com/auth/drive", "profile", "email"];
 
 export default () => {
-  const [access_token, setAccess_token] = useState(null);
-  const [userInfo, setUserInfo] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const [fileUrl, setFileUrl] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
   const [pickerUrl, setPickerUrl] = useState(null);
 
-  const [request, response, promptAsync] = Google.useAuthRequest(
-    {
-        androidClientId: "955311692015-ta1lf17m1h1sak8ce4ljjkb5qc6d7dls.apps.googleusercontent.com",
-        webClientId: GOOGLE_WEB_TOKEN,
-        scopes: ["profile", "email"],
-    },
-    {
-        authorizationEndpoint: "https://accounts.google.com/o/oauth2/v2/auth",
-    },
-  );
+  const redirectUri = AuthSession.makeRedirectUri({ native: `com.tiobilito.BitLabTDI:/oauth2redirect` });
+  
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    webClientId: GOOGLE_WEB_TOKEN,
+    androidClientId: GOOGLE_ANDROID_TOKEN,
+    scopes: ["profile", "email", "https://www.googleapis.com/auth/drive.readonly"],
+    redirectUri,
+  });
 
   useEffect(() => {
     if (response?.type === "success") {
-      setIsLoading(true);
+      console.log("[+] success")
       const { authentication } = response;
-      if (authentication) {
-          setAccess_token(authentication.accessToken);
-          fetchUserInfo(authentication.accessToken);
-      }
-    } else if (response?.type === "error") {
-      console.log("Error de autenticación: ", response.error || "Error de autenticación");
-      setIsLoading(false)
+      if (authentication?.accessToken)
+        setAccessToken(authentication.accessToken)
     }
-  }, [response]);
 
-  const fetchUserInfo = async (token) => {
-    try {
-      const response = await fetch(
-        "https://www.googleapis.com/oauth2/v2/userinfo",
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Error HTTP: " + response.status);
-      }
-
-      const user = await response.json();
-      setUserInfo(user);
-    } catch (err) {
-      console.log("Err -> ", err);
-      console.log("Err response -> ", err.response);
-      console.error("Error al intercambio de token: ", err);
-    } finally {
-      setIsLoading(false);
+    if (response?.type !== "success") {
+      console.log("[x] -> ", response)
     }
-  };
 
-  const handleGoogleSignIn = async () => {
-    try {
-        setIsLoading(true);
-        await promptAsync();
-    } catch (err) {
-      console.log("Err -> ", err);
-      console.log("Err response -> ", err.response);
-      setIsLoading(false);
+    console.log("Response -> ", response)
+    // console.log("params 1 -> ", response.params)
+    // console.log("params 2 -> ", response.params?.access_token)
+
+  }, [response])
+
+  const openGooglePicker = () => {
+    if(!accessToken) {
+      console.log("Inicia sesión")
+      return;
     }
+
+    const pickerUrl = `https://drive.google.com/embeddedfolderview?id=root&authuser=0#grid`;
+    // const pickerUrl = "https://accounts.google.com/ServiceLogin?continue=https://drive.google.com/embeddedfolderview?id=root&authuser=0";
+    setPickerUrl(pickerUrl)
   }
 
-  const getPublicFileLink = async (fileId) => {
-    try {
-      const response = await fetch(
-        `https://www.googleapis.com/drive/v3/files/${fileId}?fields=id,name,webViewLink`,
-        {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            visibility: "anyoneWithLink",
-          }),
-        }
-      );
-
-      const file = await response.json();
-      setFileUrl(file.webViewLink);
-    } catch (err) {
-      console.log("[x] Err -> ", err);
-      console.log("[x] Err response -> ", err.response);
-      console.error("Error getting public file link");
-    }
-  };
-
-  const authenticateGoogle = async () => await promptAsync();
-
-  const openGooglePicker = (token) => {
-    const pickerUrl = `https://drive.google.com/embeddedfolderview?id=root&authuser=0#grid`;
-
-    setPickerUrl(pickerUrl);
-  };
-
+  const PickerModal = ({ pickerUrl, onClose }) => {
+    return (
+      <Modal
+        visible={!!pickerUrl}
+        animationType="slide"
+        transparent={false}
+      >
+        <View style={{ flex: 1 }}>
+          <Button title="Cerrar" onPress={onClose}/>
+          <WebView source={{ uri: pickerUrl }} style={{flex: 1, marginTop: 10 }} />
+        </View>
+      </Modal>
+    )
+  }
+  
   return (
     <View style={{ padding: 20 }}>
-      {!userInfo ? (
-        <View>
-            <Text>Iniciar sesión con Google</Text>
-            <Button
-            title={isLoading ? "Cargando..." : "Iniciar sesión con Google"}
-            onPress={handleGoogleSignIn}
-            disabled={!request || isLoading}
-            />
-        </View>
+      {!accessToken ? (
+        <Button title="Iniciar sesión con Google" onPress={() => promptAsync()} />
       ) : (
         <>
-          <Text>Bienvenido, {userInfo?.name}</Text>
-          <Text>Email: {userInfo?.email}</Text>
-          <Text>Tu Access Token: {access_token}</Text>
-          <Button
-            title="Obtener enlace público de archivo"
-            onPress={() => getPublicFileLink}
-          />
-          {fileUrl && (
-            <Text>
-              Enlace público del archivo: <Text>{fileUrl}</Text>
-            </Text>
-          )}
+          <Text>Autenticado con éxito</Text>
+          <Button title="Abrir Google Picker" onPress={openGooglePicker} />
         </>
       )}
+  
+      <PickerModal pickerUrl={pickerUrl} onClose={() => setPickerUrl(null)} />
     </View>
   );
 };
